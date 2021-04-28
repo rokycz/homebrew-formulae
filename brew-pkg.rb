70c5b0ce468ef7c5e3ab6a91b2d79613b4f0012e
rokycz <58823989+rokycz@users.noreply.github.com>
2021-04-28 08:37:30 +0200

    Update brew-pkg.rb

--- brew-pkg.rb
+++ brew-pkg.rb
@@ -1,8 +1,8 @@
 class BrewPkg < Formula
   desc "Homebrew command for building OS X packages from installed formulae."
   homepage "https://github.com/timsutton/brew-pkg"
-  url "https://github.com/timsutton/brew-pkg.git", :tag => "v0.1.6",
-                                                   :revision => "ac9e3885c01dca0616fe3cec1530fc153da3271b"
+  url "https://github.com/timsutton/brew-pkg.git", :tag => "v0.1.7",
+                                                   :revision => "b689c518a316433919cdbf118e03a0f9eaf26528"
 
   # This is an .rb that must be executable in order for Homebrew to
   # find it with the 'which' method, so we skip_clean
