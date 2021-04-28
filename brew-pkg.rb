347ef6f70d6cb9f91e4004ac3e4a6c5e33e32930
rokycz <58823989+rokycz@users.noreply.github.com>
2021-04-28 08:51:57 +0200

    Update brew-pkg.rb

--- brew-pkg.rb
+++ brew-pkg.rb
@@ -1,7 +1,7 @@
 class BrewPkg < Formula
   desc "Homebrew command for building OS X packages from installed formulae."
   homepage "https://github.com/rokycz/brew-pkg"
-  url "https://github.com/timsutton/brew-pkg.git", :tag => "v0.1.7",
+  url "https://github.com/rokycz/brew-pkg.git", :tag => "v0.1.7",
                                                    :revision => "b689c518a316433919cdbf118e03a0f9eaf26528"
 
   # This is an .rb that must be executable in order for Homebrew to
